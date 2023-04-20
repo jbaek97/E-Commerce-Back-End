a150fd6063d35bd936b8e8e5e84f5363f5b94431
Joseph Baek <josephbaek97@gmail.com>
2023-04-20 09:26:41 -0700

    edited part of product route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -111,8 +111,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(productData);
+    console.log(productData);
+  } catch(err) {
+    res.status(500).json(err);}
 });
 
 module.exports = router;
